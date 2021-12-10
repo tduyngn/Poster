ec1ca5dd53ff5086a83baf0130286363a951970f
tduyngn <68032868+tduyngn@users.noreply.github.com>
2021-12-10 14:19:13 -0500

    My Poster

---
+++ Poster.js
@@ -0,0 +1,12 @@
+// $('#dudukiki').css("background","blue")
+
+// let bgColor =  $('#dudukiki').css("background-color")
+
+// $('title').text("Avan crazy late experiments")
+
+var pix = document.getElementsByClassName("pixel");
+for (var i = 0; i < pix.length; i++) {
+  pix[i].style.animationDelay = Math.ceil(Math.random()*5000)+"ms";
+}
+
+alert("scale me")
